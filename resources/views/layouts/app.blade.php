6e3bf1147952c85aaa59ccc9ae14d55ebc16afa6
xuliam <lian.xu77@gmail.com>
2020-01-09 06:43:12 +0000

    add heritage

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>BBS</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{asset('static/css/bootstrap.min.css')}}" />
+    <script src="{{asset('static/js/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{asset('static/js/popper.min.js')}}"></script>
+    <script src="{{asset('static/js/bootstrap.min.js')}}"></script>
+    <link rel="stylesheet" type="test/css" href="{{asset('static/simditor/styles/simditor.css')}}" />
+    <script type="test/javascript" src="{{asset('static/simditor/scripts/module.js')}}"></script>
+    <script type="test/javascript" src="{{asset('static/simditor/scripts/hotkeys.js')}}"></script>
+    <script type="test/javascript" src="{{asset('static/simditor/scripts/uploader.js')}}"></script>
+    <script type="test/javascript" src="{{asset('static/simditor/scripts/simditor.js')}}"></script>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
--- resources/views/msg.blade.php
+++ resources/views/msg.blade.php
@@ -1,2 +1,63 @@
+@extends('layouts.app')
 
-hello world
+@section('content')
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">Message Board</h1>
+        <p class="lead">This is a message board which powered by laravel</p>
+    </div>
+
+    <form action="{{route('save')}}" method='POST'>
+        @csrf
+        <div class='row'>
+            <div class='col-12'>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="form-group">
+                    <textarea id='content' name='content' class="form-control" rows='4'></textarea>
+                    <script>
+                        var editor = new Simditor({
+                            textarea: $('#content')
+                            //optional options
+                        });
+                    </script>
+                </div>
+            </div>
+            <div class='col-3'>
+                <div class="form-group">
+                    <input name='username' class='form-control' type='text' />
+                </div>
+            </div>
+            <div class='col-9 d-flex'>
+                <div class="form-group ml-auto">
+                    <input class='btn btn-primary' type='submit' value='提交' />
+                </div>
+            </div>
+
+        </div>
+    </form>
+
+    <div class='row'>
+        <div class='col-12'>
+            {{--                @foreach($msgs as $msg)--}}
+            <div class='border rounded p-2 mb-2'>
+                <div class='text-primary'>ok</div>
+                <div>good</div>
+            </div>
+            {{--                @endforeach--}}
+        </div>
+    </div>
+    <div class='row'>
+        <div class='col-12'>
+            11
+        </div>
+    </div>
+</div>
+@endsection
